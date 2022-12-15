cfe47e29f5a808c0aa5cf864e0b05e3199da90b6
Alberto Hanna <alberto.adell17@gmail.com>
2022-12-15 12:24:38 +0000

    landing-page added IntersectionObserver

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -1,88 +1,102 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
- * 
-*/
+ *
+ */
 
-const sections = Array.from(document.getElementsByTagName('section'));
-const nav_bar = document.getElementById('navbar__list');
+const sections = Array.from(document.getElementsByTagName("section"));
+const nav_bar = document.getElementById("navbar__list");
 const dummy_tag = document.createDocumentFragment();
-// const dummy_tag = document.getElementById('navbar__list');
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
+ *
+ */
 
-
-// fast scroll
-function scroll_maker(sec_id){
-    return (event) => {
-        event.preventDefault();
-        document.getElementById(sec_id).scrollIntoView({behavior: "smooth"});
-    }
+function scroll_maker(sec_id) {
+  return (event) => {
+    event.preventDefault();
+    document.getElementById(sec_id).scrollIntoView({ behavior: "smooth" });
+  };
 }
-
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
-// build the nav
 function build_nav() {
-    sections.forEach((element, index) => {
-        let a_tag = document.createElement('a');
-        let li_tag = document.createElement('li');
+  
+  sections.forEach((element, index) => {
 
-        a_tag.innerHTML = element.dataset.nav;
-        a_tag.classList.add("menu__link");
-        a_tag.href = `#section${index+1}`;
-        a_tag.onclick = scroll_maker(`section${index+1}`);
-        
-        li_tag.appendChild(a_tag);
-        dummy_tag.appendChild(li_tag);
-    });
-    nav_bar.appendChild(dummy_tag)
+    let a_tag = document.createElement("a");
+    let li_tag = document.createElement("li");
+
+    a_tag.innerHTML = element.dataset.nav;
+    a_tag.classList.add("menu__link");
+    a_tag.href = `#section${index + 1}`;
+    a_tag.onclick = scroll_maker(`section${index + 1}`);
+
+    li_tag.appendChild(a_tag);
+    dummy_tag.appendChild(li_tag);
+  });
+  nav_bar.appendChild(dummy_tag);
 }
 
-// Add class 'active' to section when near top of viewport
 
+function observe() {
 
-// Scroll to anchor ID using scrollTO event
+  let options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.7,
+  };
 
+  let callback = (entries) => {
+
+    if (entries[0].isIntersecting) {
+      entries[0].target.classList.add("active");
+    } else {
+      entries[0].target.classList.remove("active");
+    }
+  };
+  let observer = new IntersectionObserver(callback, options);
+  sections.forEach((element) => observer.observe(element));
+}
 
 /**
  * End Main Functions
  * Begin Events
+ *
+ */
+
+document.addEventListener("DOMContentLoaded", () => {
+  build_nav();
+  observe();
+});
+
+/**
+ * End Main Functions
  * 
-*/
-
-// Build menu 
-build_nav();
-// Scroll to section on link click
-
-// Set sections as active
-
-
+ */
