7cc266e12c4c44bfe34b6272acd68bf0fb9453a4
Alberto Hanna <alberto.adell17@gmail.com>
2022-12-15 12:27:02 +0000

    landing-page comments and style

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -33,6 +33,7 @@ const dummy_tag = document.createDocumentFragment();
  *
  */
 
+// smooth scroll
 function scroll_maker(sec_id) {
   return (event) => {
     event.preventDefault();
@@ -46,25 +47,28 @@ function scroll_maker(sec_id) {
  *
  */
 
+// build the navigation bar
 function build_nav() {
   
   sections.forEach((element, index) => {
-
+    // define block variables
     let a_tag = document.createElement("a");
     let li_tag = document.createElement("li");
-
+    // add the properties
     a_tag.innerHTML = element.dataset.nav;
     a_tag.classList.add("menu__link");
     a_tag.href = `#section${index + 1}`;
     a_tag.onclick = scroll_maker(`section${index + 1}`);
-
+    // li > a
     li_tag.appendChild(a_tag);
     dummy_tag.appendChild(li_tag);
   });
+  // add the fragment to the DOM 
+  // fragment > li > a > 
   nav_bar.appendChild(dummy_tag);
 }
 
-
+// Add class 'active' to section when near top of viewport
 function observe() {
 
   let options = {
@@ -74,14 +78,16 @@ function observe() {
   };
 
   let callback = (entries) => {
-
+    // `entries[0]` as we only use the first elementt
     if (entries[0].isIntersecting) {
       entries[0].target.classList.add("active");
     } else {
       entries[0].target.classList.remove("active");
     }
   };
+  // create observer instance
   let observer = new IntersectionObserver(callback, options);
+  // apply the observer to every section
   sections.forEach((element) => observer.observe(element));
 }
 
@@ -91,8 +97,12 @@ function observe() {
  *
  */
 
+// after the page load 
 document.addEventListener("DOMContentLoaded", () => {
+  // Build menu
+  // Scroll to section on link click
   build_nav();
+  // Set sections as active
   observe();
 });
 
