6a4560c44d29d001a6556666d27859914ebd6613
hanna-ssh <hanna@Digital-Ocean.com>
2022-12-26 07:41:13 +0000

    weather-journal server.js comments and code

--- fwd-weather-journal-app/server.js
+++ fwd-weather-journal-app/server.js
@@ -1,49 +1,51 @@
-// Setup empty JS object to act as endpoint for all routes
-const projectData = {
-  temperature: 0,
-  zip: "",
-  feelings: "",
-  date: new Date(),
-};
-
-const port = 3000;
-const apiKey = "";
-
-// Require Express to run server and routes
+/* Dependencies */
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+/* Constants */
+
+// the project data object
+const projectData = {};
+
+// the local server port
+const port = 3000;
 
 // Start up an instance of app
 const app = express();
 
 /* Middleware*/
 
-//Here we are configuring express to use body-parser as middle-ware.
+// body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
+// cors for cross origin allowance
 app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static("website"));
 
-// Setup Server
+/* Setup Server */
+
+// GET route
 app.get("/data", (req, res) => {
   res.send(projectData);
 });
 
-app.post("/data", (req, res,) => {
-    console.log(req.body);
-    
-    projectData.feelings = req.body.feelings;
-    projectData.date = req.body.date;
-    
-    res.send(projectData);
+// POST route
+app.post("/data", (req, res) => {
+  console.log(req.body);
+
+  // add the data to the projectData object
+  for (let [key, value] of Object.entries(req.body)) {
+    projectData[key] = value;
+  }
+
+  res.send(projectData);
 });
 
-// running the server on port 3000
+/* Start Server */
 app.listen(port, () => {
   console.log(`Server is running on port ${port}!`);
 });
